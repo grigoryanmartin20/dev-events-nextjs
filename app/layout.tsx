9626ca619351b5a02117dbb4e41f30be7a869b30
Martin Grigoryan <grigoryanmartin20@gmail.com>
2025-10-31 16:18:08 +0400

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
+// Styles
+import "./globals.css";
+// Components
+import LightRays from "./components/LightRays";
+import Navbar from "./components/Navbar";
+
+const schibstedGrotesk = Schibsted_Grotesk({
+	variable: "--font-schibsted-grotesk",
+	subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+	variable: "--font-martian-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "DevEvent",
+	description: "The Hub for Every Dev Event You Mustn't Miss",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${schibstedGrotesk.variable} ${martianMono.variable} antialiased min-h-screen`}>
+				{/* navbar */}
+				<Navbar />
+				{/* navbar end */}
+				<div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+					{/* light rays */}
+					<LightRays
+						raysOrigin="top-center-offset"
+						raysColor="#5dfeca"
+						raysSpeed={0.5}
+						lightSpread={0.9}
+						rayLength={1.4}
+						followMouse={true}
+						mouseInfluence={0.02}
+						noiseAmount={0}
+						distortion={0.01}
+					/>
+					{/* light rays end */}
+				</div>
+				<main>
+					{children}
+				</main>
+			</body>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+// components
+import ExploreBtn from "./components/ExploreBtn";
+import EventCard from "./components/EventCard";
+// Constants
+import { events } from "./lib/constants";
+
+export default function Home() {
+	return (
+		<div>
+			<section>
+				<h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+				<p className="text-center mt-5">Hackathons, Meetups, Conferences, All in One Place</p>
+				{/* explore btn */}
+				<ExploreBtn />
+				{/* explore btn end */}
+				<div className="mt-20 space-y-7">
+					<h3>Featured Events</h3>
+					<ul className="events list-none">
+						{events.map(event => (
+							<li key={event.title}>
+								<EventCard {...event} />
+							</li>
+						))}
+					</ul>
+				</div>
+			</section>
+		</div>
+	);
+}
